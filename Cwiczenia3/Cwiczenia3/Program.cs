8d3d85f7e224a9e8053d52bd87cb0c6aa69d9645
jakuborzelowski <s27977@pjwstk.edu.pl>
2024-03-22 20:09:13 +0100

    stworzenie 1 kontenera

--- Cwiczenia3/Cwiczenia3/Program.cs
+++ Cwiczenia3/Cwiczenia3/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Cwiczenia3.Containers;
+using Cwiczenia3.Enum;
+    
+Container container1 = new LiquidContainer(5.5,2100,2.4,5000,ContainerType.Liquid,true);
